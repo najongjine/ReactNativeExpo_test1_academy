f4c812bfd4e0b6b6c15071a7211760d39d5523a0
najongjine <najongjin3@hotmail.com>
2025-07-08 11:10:29 +0900

    .

--- app/(tabs)/myscreen.tsx
+++ app/(tabs)/myscreen.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, StyleSheet, TextInput } from "react-native";
+import { Button, ScrollView, StyleSheet, TextInput } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
@@ -11,6 +11,19 @@ interface MyMemoType {
 export default function MyScreen() {
   const [text, setText] = useState("");
   const [myMemo, setMyMemo] = useState<MyMemoType[]>([]);
+
+  const handleAddMemo = () => {
+    if (!text?.trim()) return;
+    let newMemo: MyMemoType = {
+      // 이거 문법 어렵다고 맨붕하시면 안됨. 어짜피 DB 연동 안한거라 큰 의미 없는 코드
+      id: myMemo?.length > 0 ? myMemo[myMemo.length - 1].id + 1 : 1,
+      content: text,
+    };
+    // 기존에 있는 list에서 새로운 데이터 붙여라
+    setMyMemo([...myMemo, newMemo]);
+    // text 데이터 초기화
+    setText("");
+  };
   return (
     <ScrollView
       style={styles.container}
@@ -25,6 +38,7 @@ export default function MyScreen() {
         value={text}
         onChangeText={setText}
       />
+      <Button title="눌러보세요" onPress={handleAddMemo} />
     </ScrollView>
   );
 }
