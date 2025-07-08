d29ca380bff10f22567fdeb821060e8131d34ff6
najongjine <najongjin3@hotmail.com>
2025-07-08 09:34:52 +0900

    .

--- app/(tabs)/myscreen.tsx
+++ app/(tabs)/myscreen.tsx
@@ -1,13 +1,24 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, TextInput } from "react-native";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
+import { useState } from "react";
 
 export default function MyScreen() {
+  const [text, setText] = useState("");
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text style={styles.title}>My Page</Text>
-    </View>
+      <TextInput
+        style={styles.textArea}
+        multiline={true}
+        numberOfLines={10}
+        placeholder="메모를 입력하세요"
+        value={text}
+        onChangeText={setText}
+      />
+    </ScrollView>
   );
 }
 
@@ -25,5 +36,13 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: "80%",
+  },
+  textArea: {
+    height: 150,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 10,
+    textAlignVertical: "top",
+    borderRadius: 8,
   },
 });
